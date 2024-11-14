175a89a7c64b4e9f1c0104999ed1ae742703645f
Da Fú <74943984+Arkadian404@users.noreply.github.com>
2024-11-14 22:37:47 +0700

    initial notification service with mongodb

---
+++ notification-service/src/main/java/org/zafu/notificationservice/configuration/SecurityConfig.java
@@ -0,0 +1,49 @@
+package org.zafu.notificationservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private static final String[] PUBLIC_ENDPOINTS = {
+    };
+
+    private final Decoder jwtDecoder;
+
+    public SecurityConfig(Decoder jwtDecoder) {
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_ENDPOINTS)
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+        http.oauth2ResourceServer(
+                config -> config.jwt(jwtConfig -> jwtConfig.decoder(jwtDecoder).jwtAuthenticationConverter(converter()))
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
+        return http.build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter converter() {
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthorityPrefix("");
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return converter;
+    }
+
+}
