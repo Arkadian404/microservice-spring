88ffd49f1fbc0033d375e878513923634b59e1b7
Da Fú <74943984+Arkadian404@users.noreply.github.com>
2024-11-15 21:12:14 +0700

    initial email service with brevo api

---
+++ notification-service/src/main/java/org/zafu/notificationservice/controller/EmailController.java
@@ -0,0 +1,26 @@
+package org.zafu.notificationservice.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.zafu.notificationservice.dto.ApiResponse;
+import org.zafu.notificationservice.dto.request.SendEmailRequest;
+import org.zafu.notificationservice.dto.response.EmailResponse;
+import org.zafu.notificationservice.service.EmailService;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailController {
+    EmailService emailService;
+
+    @PostMapping("/email/send")
+    public ApiResponse<EmailResponse> sendEmail(@RequestBody SendEmailRequest request){
+        return ApiResponse.<EmailResponse>builder()
+                .result(emailService.sendEmail(request))
+                .build();
+    }
+}
---
+++ notification-service/src/main/java/org/zafu/notificationservice/repo/httpClient/EmailClient.java
@@ -0,0 +1,16 @@
+package org.zafu.notificationservice.repo.httpClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.zafu.notificationservice.dto.request.EmailRequest;
+import org.zafu.notificationservice.dto.response.EmailResponse;
+
+@FeignClient(name = "email-client", url = "https://api.brevo.com")
+public interface EmailClient {
+    @PostMapping(value = "/v3/smtp/email", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmailResponse sendEmail(@RequestHeader("api-key") String apiKey, @RequestBody EmailRequest request);
+
+}
